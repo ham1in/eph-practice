d8d01c75a2b9395635ad38f65a24db54c5cb8bc8
ham1in <wuhamlin@gmail.com>
2024-09-11 11:45:53 -0400

    bug fixes, incomplete

--- ED-Holstein-1-el.py
+++ ED-Holstein-1-el.py
@@ -1,11 +1,11 @@
-# Exact diagonalization code for 1-electron Holstein model
+# Exact diagonalization code
 import numpy as np
 
 t= -1
 nbos = 10
 nsites = 64
 omega = 2
-g = 0.1
+g = 1.25
 
 H = np.zeros([(nbos+1)*nsites, (nbos+1)*nsites])
 
@@ -13,23 +13,25 @@ for i in range((nbos+1) * nsites):
   for j in range((nbos+1) * nsites):
     mat_el = 0
     if i == j:
-      mat_el += omega * (i%(nbos+1))
-    if abs(i-j)/(nbos+1) == 1 or abs(i-j)/(nbos+1) == nsites:
+      mat_el += omega * (j%(nbos+1))
+    if abs(i-j)/(nbos+1) == 1 or abs(i-j)/(nbos+1) == (nsites-1):
       mat_el += t
     if abs(i-j) == 1 and i//(nbos + 1) == j//(nbos + 1):
       if j%(nbos + 1) == 0:
-        mat_el +=g*np.sqrt(j%(nbos + 1) +1)
+        mat_el +=g
       elif j%(nbos + 1) == nbos:
+        mat_el +=g*np.sqrt(nbos)
+      elif j>i:
         mat_el +=g*np.sqrt(j%(nbos + 1))
       else:
-        mat_el +=g*(np.sqrt(j%(nbos + 1)+1) + np.sqrt(j%(nbos + 1)))
+        mat_el +=g*np.sqrt(j%(nbos +1)+1)
+
     H[i,j] = mat_el
 
-#print(H)
+print(H)
 eigenvalues, eigenvectors = np.linalg.eig(H)
 print(min(eigenvalues))
 #print(eigenvectors[:,0])
-    
 
 
 
