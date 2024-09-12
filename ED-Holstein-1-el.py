b04b096b0aae08ba44ffd35709b0e2cadd576cf2
ham1in <wuhamlin@gmail.com>
2024-09-12 11:27:52 -0400

    FIXED

--- ED-Holstein-1-el.py
+++ ED-Holstein-1-el.py
@@ -2,17 +2,17 @@
 import numpy as np
 
 t = -1
-nbos = 2
+nbos = 6
 nsites = 4
-omega = 2
-g = 0.1
+omega = 1
+g = 0.5
 
 b= np.zeros((nbos, nbos))
 for j in range(1, nbos):
     b[j-1, j] = np.sqrt(j)
 
-b_dagger = np.transpose(b)
 
+b_dagger = np.transpose(b)
 sum_b_b_dagger = b_dagger + b
 
 # No pbc
@@ -22,24 +22,44 @@ for j in range(1, nsites):
     hop[j, j-1] = t
 
 ph_occ = np.dot(b_dagger, b)
-
 Id_ph = np.eye(nbos)
 Id_el = np.eye(nsites)
 
+#Construct H_el
+H_el = np.zeros((nsites*nbos**nsites, nsites*nbos**nsites))
+H_el_tmp = hop
+for i in range(nsites):
+    H_el_tmp = np.kron(H_el_tmp, Id_ph)
 
-H_el = np.kron(hop, Id_ph)
-H_b =  np.kron(Id_el, omega* ph_occ)
-H_elph = np.zeros((nsites*nbos, nsites*nbos))
+assert(np.size(H_el_tmp) == np.size(H_el))
+H_el = H_el_tmp
+
+#Construct H_b
+H_b = np.zeros((nsites*nbos**nsites, nsites*nbos**nsites))
+for i in range(nsites):
+    mats = [Id_ph] * nsites
+    mats[i] = omega * ph_occ
+    res = Id_el
+    for mat in mats:
+        res = np.kron(res,mat)
+    H_b += res
+
+#H_b =  np.kron(Id_el, omega* ph_occ)
+H_elph = np.zeros((nsites*nbos**nsites, nsites*nbos**nsites))
 
 for i in range(nsites):
     n_i = np.zeros((nsites,nsites))
     n_i[i,i] = 1
-    el_ph = np.kron(n_i, g*sum_b_b_dagger)
+    mats = [Id_ph] * nsites
+    mats[i] = g * sum_b_b_dagger
+    el_ph = n_i
+    for mat in mats:
+        el_ph = np.kron(el_ph, mat)
     H_elph += el_ph
 
 H = H_el + H_b + H_elph
 
 #print(np.size(H))
 print("Diagonalizing H")
-eigenvalues, eigenvectors = np.linalg.eig(H)
+eigenvalues, eigenvectors = np.linalg.eigh(H)
 print((eigenvalues[0]))
--- ipie_benchmark.py
+++ ipie_benchmark.py
@@ -31,5 +31,5 @@ def ipie_fci(w0: float, g: float, t: float, nsites: int,
 
     return e[0]
 
-energy = ipie_fci(2, 0.1, 1, 4, 1, verbose=True)
+energy = ipie_fci(1, 0.5, 1, 2, 5, verbose=True)
 print(energy)
